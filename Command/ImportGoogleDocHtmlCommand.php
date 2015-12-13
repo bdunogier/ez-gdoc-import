e64953b5826c0f5df4a61c13e710d7b7da2dabc1
Bertrand Dunogier <bd@ez.no>
2015-12-13 19:20:18 +0100

    import-gdoc-html: Made the HTML file an argument

--- Command/ImportGoogleDocHtmlCommand.php
+++ Command/ImportGoogleDocHtmlCommand.php
@@ -8,6 +8,7 @@ use eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException;
 use eZ\Publish\Core\MVC\Symfony\Routing\RouteReference;
 use eZ\Publish\Core\MVC\Symfony\SiteAccess;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,15 +18,23 @@ class ImportGoogleDocHtmlCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('ez:gdoc:import-gdoc-html');
+        $this
+            ->setName('ez:gdoc:import-gdoc-html')
+            ->setDescription('Imports an HTML export of a google document to an article')
+            ->addArgument('html-file', InputArgument::REQUIRED, 'path to the HTML file to import');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $htmlFile = $input->getArgument('html-file');
+        if (!is_file($htmlFile) || !is_readable($htmlFile)) {
+            $output->writeln("<error>$htmlFile doesn't exist or is not readable</error>");
+            return 1;
+        }
         $converter = $this->getContainer()->get('ezpublish.fieldType.ezrichtext.converter.input.html');
 
         $htmlDoc = new \DOMDocument();
-        $htmlDoc->loadHTMLFile(__DIR__ . '/doc2.html');
+        $htmlDoc->loadHTMLFile($input->getArgument('html-file'));
         $docbookDoc = $converter->convert($htmlDoc);
 
         $xml = $docbookDoc->saveHTML();
